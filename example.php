967e745edec777683c97fe68f29ddebf53c08a7e
kc merrill <kcmerrill@gmail.com>
2013-01-02 15:46:10 -0700

    Fixed a namespace issue

--- example.php
+++ example.php
@@ -6,7 +6,7 @@ require_once 'events.php';
  * Just a really quick example on it's usage.
  */
 
-$event = new events;
+$event = new kcmerrill\events;
 
 /**
  * Priority 10. Doing this to show priorities and how they are run in numerical order.
